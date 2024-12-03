e7cc7dcad22c0f0eeb1382d399d15e54cbc50e65
jiyeon0926 <yeoni9094@naver.com>
2024-12-03 15:38:05 +0900

    feat: 작업원격 저장소 없이 바로 dev ãpush

--- src/main/java/git/test/test2/Jiyeon.java
+++ src/main/java/git/test/test2/Jiyeon.java
@@ -5,4 +5,10 @@ public class Jiyeon {
     private String name;
     private int age;
     private String email;
+
+    public Jiyeon(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
 }
